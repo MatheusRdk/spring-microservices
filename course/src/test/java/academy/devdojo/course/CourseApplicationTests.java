928c1450f0de4dff9b8d26ef313e09f39c60bb06
MatheusRdk <matheus_rudek@hotmail.com>
2023-07-04 20:20:32 -0300

    first commit

---
+++ course/src/test/java/academy/devdojo/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.course;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
