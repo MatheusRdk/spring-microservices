ef7180ed724b3dd99e1824b9e041bedbea79976e
MatheusRdk <matheus_rudek@hotmail.com>
2023-07-05 10:30:01 -0300

    security filter

---
+++ core/src/main/java/academy/devdojo/core/repository/ApplicationUserRepository.java
@@ -0,0 +1,10 @@
+package academy.devdojo.core.repository;
+
+
+import academy.devdojo.core.model.ApplicationUser;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ApplicationUserRepository extends PagingAndSortingRepository<ApplicationUser, Long> {
+
+    ApplicationUser findByUsername(String username);
+}
